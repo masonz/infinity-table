6e58a5f5e8ee5304c0bfbee611956e137196a161
Masonz <masonzone@gmail.com>
2018-09-28 21:08:18 +0800

    chore: Add deploy.sh

---
+++ .scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git config --global user.email "<$GH_EMAIL>"
+git config --global user.name "$GH_NAME"
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -qf https://${GH_TOKEN}@github.com/masonz/infinity-table.git master:gh-pages
+
+cd -
