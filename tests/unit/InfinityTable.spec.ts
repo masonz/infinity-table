f2a1e38648106080b7586111ff2caa77c49433a3
Masonz <masonzone@gmail.com>
2018-09-28 18:20:42 +0800

    test: Add basic unit test

--- tests/unit/InfinityTable.spec.ts
+++ tests/unit/InfinityTable.spec.ts
@@ -1,12 +1,46 @@
 import { shallowMount } from '@vue/test-utils'
 import InfinityTable from '@/components/InfinityTable.vue'
+class TestData {
+  public count: number = 0
+  public data: any[] = []
+  public columnDefs: any[] = []
 
-describe('InfinityTable.vue', () => {
-  it('renders props.msg when passed', () => {
-    const data = [{ id: 1, name: 'masonz' }]
+  constructor(count: number = 10) {
+    this.count = count
+    this.generateData()
+    this.generateColumnDefs()
+  }
+
+  private generateData() {
+    this.data = []
+    for (let i = 1; i <= this.count; i++) {
+      this.data.push({
+        id: i,
+        name: `staff_${i}`,
+      })
+    }
+  }
+
+  private generateColumnDefs() {
+    this.columnDefs = []
+    if (this.data[0]) {
+      Object.keys(this.data[0]).map((key) => {
+        this.columnDefs.push({ filed: key, title: key })
+      })
+    }
+  }
+}
+
+describe('Basic test', () => {
+  it('Render the correct number of rows', async () => {
+    const { data, columnDefs } = new TestData()
+    const height = 650
     const wrapper = shallowMount(InfinityTable, {
-      propsData: { data },
+      propsData: { data, height, columnDefs },
     })
-    expect(wrapper.findAll('tr').length).toEqual(data.length + 1)
+    await wrapper.vm.$nextTick()
+    expect(
+      wrapper.find('.table__body-middle').findAll('.infinity-table__row').length,
+    ).toEqual(data.length)
   })
 })
